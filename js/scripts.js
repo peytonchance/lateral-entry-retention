6c10e02156a6692361b2391f0fd92c03150965d8
peytonchance <peytonchance@gmail.com>
2017-11-14 14:33:04 -0500

    Updated graph for responsiveness.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 var chart = c3.generate({
+
     data: {
         x: 'x',
         columns: [
-            ['x', 'UNC System Graduate', 'NC Private University Graduate', 'Lateral Entry', 'Out of State'],
+            ['x', 'UNC System', 'NC Private', 'Lateral Entry', 'Out of State'],
             ['3-year Retention Rate', 0.8528, 0.8299, 0.6454, 0.6610],
             ['5-year Retention Rate', 0.7219, 0.6929, 0.4781, 0.4819]
         ],
